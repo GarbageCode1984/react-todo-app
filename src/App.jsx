4fbc54cf55083c526892c733c8bf5df75d1104c2
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-02-21 19:44:30 +0900

    app.jsx 수정

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+    return (
+        <div className="container">
+            <div className="todoBlock">
+                <div className="title">
+                    <h1>할 일 목록</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
