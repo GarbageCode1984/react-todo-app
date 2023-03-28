30f700a557ecaf1bf3f19de1aab63ee08516e89f
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-03-28 21:05:17 +0900

    Lists add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import "./App.scss";
 import Form from "./components/Form";
-import List from "./components/List";
+import Lists from "./components/Lists";
 
-function App() {
+const App = () => {
     const [todoDatas, setTodoDatas] = useState([]);
     const [valueText, setValueText] = useState("");
 
@@ -18,17 +18,22 @@ function App() {
         setValueText("");
     };
 
+    const handleClick = (id) => {
+        let newTodoData = todoDatas.filter((data) => data.id !== id);
+        setTodoDatas(newTodoData);
+    };
+
     return (
         <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
             <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
                 <div className="flex justify-between mb-3">
                     <h1>할 일 목록</h1>
                 </div>
-                <List todoDatas={todoDatas} setTodoDatas={setTodoDatas} />
+                <Lists todoDatas={todoDatas} setTodoDatas={setTodoDatas} handleClick={handleClick} />
                 <Form handleSubmit={handleSubmit} valueText={valueText} setValueText={setValueText} />
             </div>
         </div>
     );
-}
+};
 
 export default App;
